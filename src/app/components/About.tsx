8251ffbc06e4257ab30035ec13baeb1a67df02b5
Josh Ellis <josh@imjoshellis.com>
2020-08-18 17:39:21 -0500

    Init About component

---
+++ src/app/components/About.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+interface AboutProps {}
+
+export const About: FunctionComponent<AboutProps> = () => (
+  <div>
+    <h2 className='py-2 text-xl font-bold'>About</h2>
+  </div>
+)
+
+export default About
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,12 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import RecentRepos from './RecentRepos'
 import FeaturedRepos from './FeaturedRepos'
+import About from './About'
 
 interface MainPropTypes {}
 
 export const Main: FunctionComponent<MainPropTypes> = () => (
   <>
     <div className='max-w-xs mx-auto lg:max-w-5xl'>
+      <About />
       <FeaturedRepos />
       <RecentRepos />
     </div>
