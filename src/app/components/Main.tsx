1bd27756a788abc4ff1645e5b3b7e47cd69c4788
Josh Ellis <josh@imjoshellis.com>
2020-08-18 16:42:09 -0500

    Add FeaturedRepos to Main

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import RecentRepos from './RecentRepos'
+import FeaturedRepos from './FeaturedRepos'
 
 interface MainPropTypes {}
 
 export const Main: FunctionComponent<MainPropTypes> = () => (
   <>
     <div className='max-w-xs mx-auto lg:max-w-5xl'>
+      <FeaturedRepos />
       <RecentRepos />
     </div>
   </>
