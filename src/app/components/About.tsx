843ab06c20ac04172a99a1fd37bdb1c0ac02d08b
Josh Ellis <josh@imjoshellis.com>
2020-08-18 17:46:18 -0500

    Add user data

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,11 +1,33 @@
 import React, { FunctionComponent } from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_ABOUT = gql`
+  query GetAbout {
+    viewer {
+      name
+      bio
+      status {
+        message
+      }
+      company
+      isHireable
+    }
+  }
+`
 
 interface AboutProps {}
 
-export const About: FunctionComponent<AboutProps> = () => (
-  <div>
-    <h2 className='py-2 text-xl font-bold'>About</h2>
-  </div>
-)
+export const About: FunctionComponent<AboutProps> = () => {
+  const { loading, error, data } = useQuery(GET_ABOUT)
+  const my = data && data.viewer
+  return (
+    <div>
+      <h2 className='py-2 text-xl font-bold'>About Josh Ellis</h2>
+      {loading && 'Loading data from GitHub...'}
+      {error && `Error! ${error.message}`}
+      {data && <div>{my.status.message}</div>}
+    </div>
+  )
+}
 
 export default About
