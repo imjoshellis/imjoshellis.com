bf4f3cddc1c01927565a028b7d99c4242cc75d03
Josh Ellis <josh@imjoshellis.com>
2020-08-18 18:13:34 -0500

    Add about data

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -5,8 +5,12 @@ const GET_ABOUT = gql`
   query GetAbout {
     viewer {
       name
+      updatedAt
       bio
+      avatarUrl
+      url
       status {
+        emojiHTML
         message
       }
       company
@@ -25,7 +29,34 @@ export const About: FunctionComponent<AboutProps> = () => {
       <h2 className='py-2 text-xl font-bold'>About Josh Ellis</h2>
       {loading && 'Loading data from GitHub...'}
       {error && `Error! ${error.message}`}
-      {data && <div>{my.status.message}</div>}
+      {data && (
+        <div className='flex items-center'>
+          <div className=''>
+            <img
+              src={my.avatarUrl}
+              className='w-64 rounded-full'
+              alt='My Avatar'
+            />
+          </div>
+          <div className='ml-4 text-xl'>
+            <span>{my.bio}</span>
+            <span className='ml-1'>I am</span>
+            {!my.isHireable && <span className='ml-1'>not</span>}
+            <span className='ml-1'>actively seeking opportunities.</span>
+            {my.status && (
+              <>
+                <span className='ml-1'>According to my GitHub status,</span>
+                <span className='ml-1'>I'm currently</span>
+                <span className='ml-1'>{my.status.message}</span>.
+                <span
+                  className='inline-block ml-1'
+                  dangerouslySetInnerHTML={{ __html: my.status.emojiHTML }}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
