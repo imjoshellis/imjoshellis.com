2ab42504ea75b5bef486deb8553f9171a7236497
Josh Ellis <josh@imjoshellis.com>
2020-08-18 11:41:30 -0500

    Add data loading to main

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,19 +1,74 @@
+import { gql, useQuery } from '@apollo/client'
 import React, { FunctionComponent } from 'react'
 import Card from './Card'
 
 interface MainPropTypes {}
 
 export const Main: FunctionComponent<MainPropTypes> = () => {
+  const GET_RECENT_REPOS = gql`
+    query GetRecentRepos {
+      viewer {
+        repositories(
+          orderBy: { field: PUSHED_AT, direction: ASC }
+          last: 3
+          privacy: PUBLIC
+        ) {
+          edges {
+            node {
+              ... on Repository {
+                name
+                description
+                homepageUrl
+                pushedAt
+                url
+                languages(first: 100) {
+                  edges {
+                    node {
+                      name
+                      color
+                    }
+                  }
+                }
+                repositoryTopics(first: 100) {
+                  edges {
+                    node {
+                      topic {
+                        name
+                      }
+                    }
+                  }
+                }
+                pullRequests(first: 100) {
+                  totalCount
+                }
+                defaultBranchRef {
+                  target {
+                    ... on Commit {
+                      history {
+                        totalCount
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+
   let classes = {
     base: ''
   }
+
+  const { loading, error, data } = useQuery(GET_RECENT_REPOS)
+
   return (
     <div className='max-w-xs mx-auto grid gap-8'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {loading && 'Loading...'}
+      {error && `Error! ${error.message}`}
+      {data && <Card />}
     </div>
   )
 }
