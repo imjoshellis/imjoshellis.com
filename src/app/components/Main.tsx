c8ca6cad0eea34a99ea6e6034b959c5404d0e7be
Josh Ellis <josh@imjoshellis.com>
2020-08-18 16:15:36 -0500

    Cleanup formatting

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,23 +1,14 @@
-import { gql, useQuery } from '@apollo/client'
 import React, { FunctionComponent } from 'react'
-import Card from './Card'
-import moment from 'moment'
 import RecentRepos from './RecentRepos'
 
 interface MainPropTypes {}
 
-export const Main: FunctionComponent<MainPropTypes> = () => {
-  let classes = {
-    base: ''
-  }
-
-  return (
-    <>
-      <div className='max-w-xs mx-auto lg:max-w-5xl'>
-        <RecentRepos />
-      </div>
-    </>
-  )
-}
+export const Main: FunctionComponent<MainPropTypes> = () => (
+  <>
+    <div className='max-w-xs mx-auto lg:max-w-5xl'>
+      <RecentRepos />
+    </div>
+  </>
+)
 
 export default Main
