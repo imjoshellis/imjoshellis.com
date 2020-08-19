972b714744ccebecd52eb5b7252b9db219ce6c45
Josh Ellis <josh@imjoshellis.com>
2020-08-18 19:11:07 -0500

    Sort tag types

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -11,16 +11,13 @@ export const Tag: FunctionComponent<TagProps> = ({ name }) => {
     ] as String[]
     const baseColor = 'bg-gray-70 text-gray-30'
     const specialNames = [
-      {
-        name: 'ruby on rails',
-        classes: 'bg-red-70 text-red-20'
-      },
-      { name: 'react', classes: 'bg-teal-60 text-teal-20' },
       { name: 'apollo', classes: 'bg-purple-70 text-purple-20' },
-      { name: 'redux', classes: 'bg-purple-70 text-purple-20' },
       { name: 'graphql', classes: 'bg-pink-60 text-pink-20' },
-      { name: 'svelte', classes: 'bg-orange text-red-90' },
       { name: 'javascript', classes: 'bg-yellow text-red-90' },
+      { name: 'react', classes: 'bg-teal-60 text-teal-20' },
+      { name: 'redux', classes: 'bg-purple-70 text-purple-20' },
+      { name: 'ruby on rails', classes: 'bg-red-70 text-red-20' },
+      { name: 'svelte', classes: 'bg-orange text-red-90' },
       { name: 'typescript', classes: 'bg-cyan-60 text-cyan-20' }
     ]
     const idx = specialNames.findIndex(o => o.name === name)
