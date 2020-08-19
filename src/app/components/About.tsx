df00ac5c657587bc31ef5537d7a959120d93e89c
Josh Ellis <josh@imjoshellis.com>
2020-08-18 20:53:00 -0500

    Update about info and style

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { gql, useQuery } from '@apollo/client'
+import IconDetail from './IconDetail'
+import { Newspaper, AcademicCap } from '../../assets/heroicons/outline'
 
 const GET_ABOUT = gql`
   query GetAbout {
@@ -35,26 +37,56 @@ export const About: FunctionComponent<AboutProps> = () => {
           <div className=''>
             <img
               src={my.avatarUrl}
-              className='w-64 rounded-full'
+              className='w-40 rounded-full'
               alt='My Avatar'
             />
           </div>
-          <div className='ml-4 text-xl'>
-            <span>{my.bio}</span>
-            <span className='ml-1'>I am</span>
-            {!my.isHireable && <span className='ml-1'>not</span>}
-            <span className='ml-1'>actively seeking opportunities.</span>
-            {my.status && (
-              <>
-                <span className='ml-1'>According to my GitHub status,</span>
-                <span className='ml-1'>I'm currently</span>
-                <span className='ml-1'>{my.status.message}</span>.
-                <span
-                  className='inline-block ml-1'
-                  dangerouslySetInnerHTML={{ __html: my.status.emojiHTML }}
-                />
-              </>
-            )}
+          <div className='flex flex-col ml-4 text-xl'>
+            <div>
+              <span>{my.bio}</span>
+            </div>
+            <div className='flex flex-col text-base'>
+              <div className='flex text-gray-30'>
+                {!my.isHireable ? (
+                  <>
+                    <span role='img' aria-label='zzz emoji' className='mr-1'>
+                      💤️
+                    </span>
+                    <span className='ml-1'>
+                      Not actively looking for work, but let's connect anyway.
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span role='img' aria-label='eyes emoji' className='mr-1'>
+                      👀
+                    </span>
+                    <span className='ml-1'>
+                      Actively looking for a new opportunity as a frontend dev
+                      for a productivity startup.
+                    </span>
+                  </>
+                )}
+              </div>
+              {my.status && (
+                <div className='flex text-gray-30'>
+                  <span
+                    role='img'
+                    aria-label='Graduate Emoji'
+                    className='mr-1'
+                    dangerouslySetInnerHTML={{ __html: my.status.emojiHTML }}
+                  />
+                  <span className='ml-1'>I'm currently</span>
+                  <span className='ml-1'>{my.status.message}</span>.
+                </div>
+              )}
+              <div className='flex text-gray-30'>
+                <span role='img' aria-label='Graduate Emoji' className='mr-1'>
+                  👨🏻‍🎓
+                </span>
+                <span className='ml-1'>Flatiron School 8/2020</span>
+              </div>
+            </div>
           </div>
         </div>
       )}
