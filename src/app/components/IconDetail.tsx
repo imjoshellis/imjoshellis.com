a35d3d4b6e0f148cc01075b47ec754a22bb4c172
Josh Ellis <josh@imjoshellis.com>
2020-08-19 07:59:53 -0500

    Add margin to IconDetail

--- src/app/components/IconDetail.tsx
+++ src/app/components/IconDetail.tsx
@@ -32,7 +32,7 @@ export const IconDetail: FunctionComponent<IconDetailProps> = ({
     {/* empty div with all possible classes for tailwindcss build purge */}
     <div className='hidden w-2 w-3 w-4 w-5 w-6 h-2 h-3 h-4 h-5 h-6 text-xs text-sm text-lg text-xl text-md ' />
     <div
-      className={`flex relative items-center text-${size} leading-4 text-gray-30`}
+      className={`flex relative items-center text-${size} leading-4 text-gray-30 mt-1`}
     >
       <div className={`w-${toNum(size)} h-${toNum(size)} mr-1 text-green-40`}>
         {icon}
