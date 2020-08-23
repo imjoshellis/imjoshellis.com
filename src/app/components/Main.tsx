ae4aedaadf45347544e0bcbbcaf9caa82d231aaf
Josh Ellis <josh@imjoshellis.com>
2020-08-23 09:38:28 -0500

    Update responsive sizing

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -6,13 +6,11 @@ import About from './About'
 interface MainPropTypes {}
 
 export const Main: React.FC<MainPropTypes> = () => (
-  <>
-    <div className='max-w-xs mx-auto lg:max-w-5xl'>
-      <About />
-      <FeaturedRepos />
-      <RecentRepos />
-    </div>
-  </>
+  <div className='max-w-xs mx-auto md:max-w-sm lg:max-w-5xl'>
+    <About />
+    <FeaturedRepos />
+    <RecentRepos />
+  </div>
 )
 
 export default Main
