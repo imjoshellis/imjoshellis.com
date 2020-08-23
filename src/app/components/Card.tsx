abab441353e279a59c8c086de8d1f00bffb72310
Josh Ellis <josh@imjoshellis.com>
2020-08-23 11:27:02 -0500

    Update layout and styles

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -38,13 +38,13 @@ export const Card: React.FC<CardPropTypes> = ({ repoData, useImage }) => {
   let classes = {
     wrap: 'shadow h-full flex flex-col',
     base:
-      'bg-gray-90 rounded-lg rounded-b-none overflow-hidden flex-grow flex flex-col',
-    head: 'flex flex-col justify-between text-gray-30 bg-gray-80',
+      'bg-gray-90 rounded-lg rounded-b-none overflow-hidden flex-grow flex flex-col gap-4',
+    head: 'flex flex-col justify-between text-gray-30 pb-4 gap-4 bg-gray-80',
     lastCommit: 'text-xs text-white pt-1',
-    tags: 'flex flex-row flex-wrap p-4 pb-2',
+    tags: 'flex flex-row flex-wrap p-4 gap-2 pb-2',
     info: {
       base: 'pb-4',
-      header: 'pl-4 pt-3 font-medium tracking-wider mb-1 text-white truncate ',
+      header: 'pl-4 font-medium tracking-wider mb-1 text-white truncate ',
       description: 'px-4 text-xs text-gray-10 flex-grow',
       stats:
         'flex flex-col justify-center pl-4 border-l border-solid border-gray-100 text-gray-30 px-4',
@@ -100,25 +100,25 @@ export const Card: React.FC<CardPropTypes> = ({ repoData, useImage }) => {
                   <Tag key={t} name={t.split('-').join(' ')} />
                 ))}
           </div>
-        </div>
-        {repoData.usesCustomOpenGraphImage && useImage && (
-          <div className='px-4 pb-4 bg-gray-80'>
-            <div className='overflow-hidden bg-gray-100 rounded p-2px'>
-              <div className='flex p-1'>
-                <div className='w-1.5 h-1.5 rounded-full bg-gray-70' />
-                <div className='w-1.5 h-1.5 ml-1 rounded-full bg-gray-70' />
-                <div className='w-1.5 h-1.5 ml-1 rounded-full bg-gray-70' />
-              </div>
-              <div className='relative pb-1/2'>
-                <img
-                  src={repoData.openGraphImageUrl}
-                  alt={repoName + ' image'}
-                  className='absolute object-cover w-full h-full rounded-b-sm'
-                />
+          {repoData.usesCustomOpenGraphImage && useImage && (
+            <div className='px-4'>
+              <div className='overflow-hidden bg-gray-100 rounded p-2px'>
+                <div className='flex p-1'>
+                  <div className='w-1.5 h-1.5 rounded-full bg-gray-70' />
+                  <div className='w-1.5 h-1.5 ml-1 rounded-full bg-gray-70' />
+                  <div className='w-1.5 h-1.5 ml-1 rounded-full bg-gray-70' />
+                </div>
+                <div className='relative pb-1/2'>
+                  <img
+                    src={repoData.openGraphImageUrl}
+                    alt={repoName + ' image'}
+                    className='absolute object-cover w-full h-full rounded-b-sm'
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
         <div className='flex flex-col flex-grow'>
           <h3 className={classes.info.header}>
             <a
