aaf175b12457121e033e6a04a0415438a835a2ea
Josh Ellis <josh@imjoshellis.com>
2020-08-26 09:21:08 -0500

    Add header

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -3,11 +3,13 @@ import RecentRepos from './RecentRepos'
 import FeaturedRepos from './FeaturedRepos'
 import About from './About'
 import BlogPosts from './BlogPosts'
+import Header from './Header'
 
 interface MainPropTypes {}
 
 export const Main: React.FC<MainPropTypes> = () => (
   <div className='max-w-xs py-8 mx-auto md:max-w-sm lg:max-w-5xl'>
+    <Header />
     <About />
     <FeaturedRepos />
     <RecentRepos />
