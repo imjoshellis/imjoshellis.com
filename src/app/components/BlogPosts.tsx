4a9a6ed2aa084678d7045f3f1203103f6eac84d0
Josh Ellis <josh@imjoshellis.com>
2020-08-26 07:32:56 -0500

    Add proper typing

--- src/app/components/BlogPosts.tsx
+++ src/app/components/BlogPosts.tsx
@@ -4,32 +4,71 @@ import Tag from './Tag'
 
 interface BlogPostsProps {}
 
+interface DevToArticle {
+  type_of: string
+  id: number
+  title: string
+  description: string
+  cover_image: string
+  readable_publish_date: string
+  social_image: string
+  tag_list: string[]
+  tags: string
+  slug: string
+  path: string
+  url: string
+  canonical_url: string
+  comments_count: number
+  public_reactions_count: number
+  collection_id: number
+  created_at: string
+  edited_at: string
+  crossposted_at: string
+  published_at: string
+  last_comment_at: string
+  published_timestamp: string
+  user: {
+    name: string
+    username: string
+    twitter_username: string
+    github_username: string
+    website_url: string
+    profile_image: string
+    profile_image_90: string
+  }
+  organization: {
+    name: string
+    username: string
+    slug: string
+    profile_image: string
+    profile_image_90: string
+  }
+}
+
+interface DevToArticles extends Array<DevToArticle> {}
+
 export const BlogPosts: React.FC<BlogPostsProps> = () => {
-  const [state, setState] = useState<any[]>([])
+  const [state, setState] = useState<DevToArticles | undefined>(undefined)
   const DEVTO_API_URL = 'https://dev.to/api/'
 
   useEffect(() => {
     fetch(DEVTO_API_URL + 'articles?username=imjoshellis', {})
       .then(res => res.json())
-      .then((json: any[]) => setState(json))
+      .then((json: DevToArticles) => setState(json))
   }, [])
-
-  if (state.length > 0) {
-    console.log(state)
-  }
 
   return (
     <div className='py-4'>
       <h2 className='flex flex-col items-baseline py-2 text-xl font-bold md:gap-2 md:flex-row'>
         Blog Posts{' '}
-        {state.length > 0 && (
+        {state !== undefined && (
           <div className='text-sm font-normal text-gray-30'>
             (last post {moment(state[0].published_at).fromNow()})
           </div>
         )}
       </h2>
       <div className='flex flex-col gap-4 md:grid md:grid-cols-2'>
-        {state.length === 0
+        {state === undefined
           ? 'Loading posts from DEV.to'
           : state.map(p => (
               <div className='flex flex-col gap-2'>
