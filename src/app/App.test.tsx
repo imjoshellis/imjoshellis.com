e442c5d0f850e2f656d3e0a4fec710ab8d556174
Josh Ellis <josh@imjoshellis.com>
2020-09-12 11:37:20 -0500

    Remove test boilerplate

--- src/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from './App'
-import { render } from './node_modules/@testing-library/react'
-import React from './node_modules/react'
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
